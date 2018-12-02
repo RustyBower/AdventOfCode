5c2944a62dbe327f3de6ea746e148ad484684230
Rusty Bower <rusty@rustybower.com>
2018-12-02 12:34:20 -0600

    Day 2 Part 2

---
+++ day2part2.py
@@ -0,0 +1,20 @@
+# I don't like this, but it's quick and dirty
+string_list = []
+
+with open('day2.txt') as f:
+  for line in f:
+    string_list.append(line.strip())
+
+for first_string in string_list:
+  for second_string in string_list:
+    diff_count = 0
+    for a, b in zip(first_string, second_string):
+      if a != b:
+        diff_count += 1
+    if diff_count == 1:
+      common_letters = ""
+      for a, b in zip(first_string, second_string):
+        if a == b:
+          common_letters += a
+      print(common_letters)
+
